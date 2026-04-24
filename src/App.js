b3701d951067231aa12668d0b2d114f97c749708
xxklusivenana <devwithjaay@gmail.com>
2026-04-23 23:56:02 -0400

    push file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import HelloWorld from './components/HelloWorld.jsx';
+import Counter from './components/Counter.jsx';
+import FoodList from './components/List.jsx';
+import Spacer from './components/Spacer.jsx';
+import FilteredList from './components/FilteredList.jsx';
+import Header from './components/Header.jsx';
+
+const produce = [
+    { name: "Apple", type: "Fruit" },
+    { name: "Banana", type: "Fruit" },
+    { name: "Carrot", type: "Vegetable" },
+    { name: "Broccoli", type: "Vegetable" },
+    { name: "Orange", type: "Fruit" },
+    { name: "Spinach", type: "Vegetable" }
+];
+
+class App extends Component {
+  render() {
+    return (
+      <div className = "App">
+        <HelloWorld/>
+        <Counter/>
+        <FilteredList items={produce}/>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/Counter.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            count: 5
+        };
+    }
+
+    incrementCount = () => {
+        this.setState({
+            count: this.state.count + 1
+        });
+    }
+  
+    render() {
+        return (
+            <div className="counter">
+                <p>{this.state.count}</p>
+                <button onClick={this.incrementCount}>
+                    Increment
+                </button>
+            </div>
+        );
+    }
+}
+
+export default Counter;
